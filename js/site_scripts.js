db9d4a4af491081296fc7cf6e9f805a2de014e38
zipofar <zipofar@example.com>
2017-02-17 10:59:08 +0300

    birthday function work

--- js/site_scripts.js
+++ js/site_scripts.js
@@ -30,19 +30,60 @@ function renderPersonBirthdat(selector) {
         day: 'numeric',
     };
 	var date_str = date.toLocaleString("ru", options).slice(-100, -3);
-	var dataBirthday = getDataBirthday(date);
-	console.log(dataBirthday.years);
+	var dataBirthday = getInfoBirthday(date);
+	console.log(dataBirthday);
 
 	$("#birthday_str").html(date_str + ", " + dataBirthday.years + " лет");
-	$("#birthday_reminder").html("2 месяца и 13 дней до ДР");
+
+	var kind_day = getKindOfDay (dataBirthday.days);
+	var kind_month = getKindOfMonth (dataBirthday.months);
+
+	if(dataBirthday.days == 0) {
+        $("#birthday_reminder").html("СЕГОДНЯ ДР");
+	}
+	else if(dataBirthday.months == 0) {
+        $("#birthday_reminder").html(dataBirthday.days + " " + kind_day + " до ДР");
+	}
+	else {
+        $("#birthday_reminder").html(dataBirthday.months + " " + kind_month + " и " + dataBirthday.days + " " + kind_day + " до ДР");
+	}
+
+		// $("#birthday_reminder").html("2 месяца и 13 дней до ДР");
 
 }
 
-function getDataBirthday(date) {
-	// date = new Date(1986, 1, 1);
+function getKindOfDay (day) {
+
+    var kind_day = "";
+
+	if(day == 1 || day == 21 || day == 31) { kind_day = "день"; }
+    else if(day >= 2 && day <= 4) { kind_day = "дня"; }
+    else if(day >= 22 && day <= 24) { kind_day = "дня"; }
+    else { kind_day = "дней"; }
+
+    return kind_day;
+}
+
+function getKindOfMonth (month) {
+
+    var kind_month = "";
+
+    if(month == 1) { kind_month = "месяц"}
+    else if(month > 1 && month < 5) { kind_month = "месяца" }
+    else if(month > 4) { kind_month = "месяцев"  }
+
+    return kind_month;
+}
+
+function getInfoBirthday(date) {
+
+	// date = new Date(1986, 7, 14);
 
 	var brth_year = date.getFullYear();
 	var curent_year = new Date().getFullYear();
+	var curent_month = new Date().getMonth();
+	var curent_date = new Date().getDate();
+
 
 	/*
 	Определяем количество лет
@@ -62,20 +103,22 @@ function getDataBirthday(date) {
     var days = "";
 	var tmp = "";
 
-    if( dateCurrentBirth > new Date() ) {
+    if( dateCurrentBirth.getDate() == curent_date && dateCurrentBirth.getMonth() == curent_month ) {
+        months = 0;
+        days = 0;
+    }
+    else if( dateCurrentBirth > new Date() ) {
 
         tmp = new Date(dateCurrentBirth - new Date());
         months = tmp.getMonth();
         days = tmp.getDate();
-
-
-	} else if( dateCurrentBirth < new Date() ) {
+	}
+	else if( dateCurrentBirth < new Date() ) {
 
     	dateCurrentBirth.setFullYear(curent_year + 1); //Если день рождения уже прошел, то к дате дня рождения в текущем году прибавим год
         tmp = new Date(dateCurrentBirth - new Date());
         months = tmp.getMonth();
         days = tmp.getDate();
-
 	}
 
 	return {years: years, months: months, days: days};
